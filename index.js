88dee8a0b1416cd67c460264e07f03d4d4450ac0
Sam Schantz <samvschantz@gmail.com>
2018-04-12 19:31:20 +0000

    Finally solved!

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  var result = 0;
+  console.log('shouldnt result have been reset to 0? ' + result)
+    array.forEach((item) =>{
+      if (typeof item !== 'number'){
+        result += sumItems(item)
+      } else {
+        result += item
+      }
+      console.log('this is result within forEach')
+    })
+    return result;
 }
 
 module.exports = sumItems;
+
